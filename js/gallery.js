46f1ccbd54544472424f7fccd521ac7de2713a0e
hyodo_soichiro <hyodo.soichiro@ethergram.jp>
2018-10-25 15:22:50 +0900

    Add Gallery function

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,30 @@
 function activateGallery() {
-  alert("Hello from the gallery file!");
+	let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
+	let mainImg = document.querySelector("#gallery-photo img");
+	let imgInfo = document.querySelector("#gallery-info > .description");
+	let imgTitle = document.querySelector("#gallery-info > .title");
+	// alert(mainImg);
+
+
+  thumbnails.forEach(function(el) {
+  	// set the Image
+  	el.addEventListener("click", function() {
+  		let newImgUrl = el.dataset.largeVersion;
+  		mainImg.setAttribute("src", newImgUrl);
+  		let currentClass = "current";
+
+  		//move .current
+  		document.querySelector(".current").classList.remove(currentClass);
+  		el.parentNode.classList.add(currentClass);
+
+  		//add title and description
+  		let imgDescription = el.dataset.description;
+  		let imgInfoTitle = el.dataset.title;
+  		imgInfo.innerHTML = imgDescription;
+  		imgTitle.innerHTML = imgInfoTitle;
+
+  	});
+
+  });
 }
+
